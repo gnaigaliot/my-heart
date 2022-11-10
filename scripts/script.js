a9eef126783a749d4c3fc89e549f3d24c7fb2257
gnaigaliot <hoanggiang521999@gmail.com>
2022-11-10 23:18:11 +0700

    update logic

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,29 @@
 $(function() {
+  // ready document
+  $(".classic-1").hide();
+  $(".classic-2").hide();
+  $(".dots-8").hide();
+  let isAvailable = false;
+
   $(".heart").on("click", function() {
+    if (isAvailable) {
+      return
+    } else {
+      isAvailable = true;
+    }
+    
     $(this).toggleClass("is-active");
+    $(".film").hide();
+    $(".classic-1").show();
+    
+    setTimeout(function() {
+      $(".classic-1").hide();
+      $(".dots-8").show();
+      
+      setTimeout(function() {
+        $(".dots-8").hide();
+        $(".classic-2").show();
+      }, 5000);
+    }, 5000);
   });
 });
